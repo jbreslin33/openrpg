f5ddfb772a565e05cbb7fd7d40b3dfc0d10c0ef0
Jim Breslin <jbreslin33@gmail.com>
2017-05-07 23:52:57 -0400

    inserts

--- src/database/insert.sql
+++ src/database/insert.sql
@@ -1,2 +1,12 @@
+--users
 insert into users (username,password) values ('jbreslin','Iggles_13');
+
+--race
+insert into race (name) values ('human');
+
+insert into class (name) values ('fighter');
+
+--characters
+insert into characters (name,user_id,race_id,class_id,x,y,z,d,full_hitpoints,current_hitpoints,level,experience) values ('Julius',1,1,1,0,0,0,0,13,13,1,0);
+
 
