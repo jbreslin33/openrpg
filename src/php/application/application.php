0ec4f3cb90b862ae635ae8461af30c185e421585
Jim Breslin <jbreslin33@gmail.com>
2017-05-08 21:08:50 -0400

    clutter

--- src/php/application/application.php
+++ src/php/application/application.php
@@ -3,32 +3,16 @@ include_once(getenv("DOCUMENT_ROOT") . "/src/php/fsm/state_machine.php");
 include_once(getenv("DOCUMENT_ROOT") . "/src/php/fsm/state.php");
 include_once(getenv("DOCUMENT_ROOT") . "/src/php/application/states/states.php");
 
-//start new session
-//session_start();
-?>
-
-<?php
-
 class Application 
 {
 
 function __construct()
 {
-
-	error_log("APPLICATION CONSTRUCTOR");
-
-	//init
-	$this->run_once = false;
-
-	$this->mInitialized = 0;
-
-	$this->mDataArray = array();
-	$this->mCode = 0;
-	$this->mRawData = 0;
 	$this->mLogs = true;
+
+	//states
 	$this->mApplicationStateMachine = new StateMachine($this);
-        
-	//admin
+
        	$this->mGLOBAL = new GLOBAL_STATE($this);
         $this->mINIT   = new INIT  ($this);
         $this->mADMIN  = new ADMIN ($this);
